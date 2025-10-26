24a06ffafc7d4ff01ab44adc9585173e9f059576
som0309 <eunseolim1018@naver.com>
2025-10-27 01:18:25 +0900

    fix: update CORS settings

--- src/main/java/com/kakaotechbootcamp/community/config/WebConfig.java
+++ src/main/java/com/kakaotechbootcamp/community/config/WebConfig.java
@@ -1,16 +1,16 @@
 package com.kakaotechbootcamp.community.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500") // Live Server 주소
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE")
                 .allowCredentials(true);
     }
