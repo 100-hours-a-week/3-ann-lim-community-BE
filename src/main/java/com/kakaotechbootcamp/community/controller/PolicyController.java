c3c590ec4264cdf9ba59e88b5c2b45885079eaf8
som0309 <eunseolim1018@naver.com>
2025-11-16 14:26:40 +0900

    chore: frontend base-url 새로운 Elastic IP로 변경

--- src/main/java/com/kakaotechbootcamp/community/controller/PolicyController.java
+++ src/main/java/com/kakaotechbootcamp/community/controller/PolicyController.java
@@ -1,18 +1,25 @@
 package com.kakaotechbootcamp.community.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PolicyController {
 
+    @Value("${frontend.base-url}")
+    private String frontendBaseUrl;
+
     @GetMapping("/terms")
-    public String terms() {
+    public String terms(Model model) {
+        model.addAttribute("frontendBaseUrl", frontendBaseUrl);
         return "terms";
     }
 
     @GetMapping("/privacy")
-    public String privacy() {
+    public String privacy(Model model) {
+        model.addAttribute("frontendBaseUrl", frontendBaseUrl);
         return "privacy";
     }
 }
